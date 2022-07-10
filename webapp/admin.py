c3924a122fed8ded3b3aca956417026a93005894
azamatdoronov <aza3el@inbox.ru>
2022-07-10 23:52:33 +0600

    Создал проект, также - файлы gitignore, requirements.txt, зарегистрировал приложение webapp, создал суперюзера с логином и паролем, сделал первую миграцию, делаю первый commit

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Issue
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type', 'created_at']
+    list_display_links = ['summary']
+    list_filter = ['summary']
+    search_fields = ['id', 'summary']
+    fields = ['id', 'summary', 'description', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Issue, IssueAdmin)
