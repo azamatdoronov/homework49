81554478f646c3ed3487f3d1ac41a2ff5be7c0ec
azamatdoronov <aza3el@inbox.ru>
2022-07-10 23:57:11 +0600

    Создал html страницы для отображения соответствующей информации, а также для применения CRUD

---
+++ webapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import widgets
+
+from webapp.models import Status, Type
+
+
+class IssueForm(forms.Form):
+    summary = forms.CharField(max_length=50, required=False, label='Summary')
+    description = forms.CharField(max_length=3000, required=True, label="Description",
+                                  widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), label='Type')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Status')
