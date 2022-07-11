e27bb5e01f288ded74e2b3394b860d10fb12ac89
azamatdoronov <aza3el@inbox.ru>
2022-07-11 23:09:10 +0600

    Внес некоторые изменения в forms.py

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from django.forms import widgets
 
 from webapp.models import Issue, Status, Type
 
 
 class IssueForm(forms.ModelForm):
+    summary = forms.CharField(max_length=50, required=True, label='Summary')
+    description = forms.CharField(max_length=3000, required=True, label='Description',
+                                  widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
     type = forms.ModelChoiceField(queryset=Type.objects.all())
     status = forms.ModelChoiceField(queryset=Status.objects.all())
 
