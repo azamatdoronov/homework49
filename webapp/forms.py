2493f602331e668325a9be0edf33ac311b782545
azamatdoronov <aza3el@inbox.ru>
2022-07-12 14:16:20 +0600

    Внес некоторые изменения в отображение соответствующих полей

--- webapp/forms.py
+++ webapp/forms.py
@@ -5,11 +5,11 @@ from webapp.models import Issue, Status, Type
 
 
 class IssueForm(forms.ModelForm):
-    summary = forms.CharField(max_length=50, required=True, label='Summary')
-    description = forms.CharField(max_length=3000, required=True, label='Description',
+    summary = forms.CharField(max_length=50, required=True, label='Заголовок:')
+    description = forms.CharField(max_length=3000, required=True, label='Описание:',
                                   widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
-    type = forms.ModelChoiceField(queryset=Type.objects.all())
-    status = forms.ModelChoiceField(queryset=Status.objects.all())
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=False, label='Тип:')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False, label='Статус:')
 
     class Meta:
         model = Issue
