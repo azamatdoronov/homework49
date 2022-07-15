c564f06f534658790dd339acfccdbf72bc99e5d2
azamatdoronov <aza3el@inbox.ru>
2022-07-15 12:21:46 +0600

    Прописал валидаторы в my_validate.py на соответствующие поля

---
+++ webapp/my_validate.py
@@ -0,0 +1,32 @@
+from django.core.exceptions import ValidationError
+
+from webapp.models import STATUSES
+
+SPECIAL_WORDS = ['bullshit', 'fuck', 'motherfucker', 'wtf']
+SPECIAL_CHARS = ['%', '*', '@', '#']
+
+
+def special_words(text):
+    for i in SPECIAL_WORDS:
+        if i.lower() in text.lower():
+            raise ValidationError(f'Description should not contain "{i}!". Forbidden words {SPECIAL_WORDS}')
+
+
+def special_chars(name):
+    for i in SPECIAL_CHARS:
+        if i in name:
+            raise ValidationError(f'Summary should not contain "{i}"!. Forbidden symbols {SPECIAL_CHARS}')
+
+
+def check_status(status):
+    list_st = []
+    for i in STATUSES:
+        list_st.append(i[0])
+    list_st = (', ').join(list_st)
+    if status in STATUSES:
+        raise ValidationError(f'"status" value must be "{list_st}"')
+
+
+def check_count(list):
+    if len(list) > 5:
+        raise ValidationError(f'"type" cannot be more than 3')
