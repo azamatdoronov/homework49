ec06e834704d56c6684f14906f521229a9250303
azamatdoronov <aza3el@inbox.ru>
2022-07-22 00:41:49 +0600

    Внес изменения связанные с CreateIssue.as_view(), UpdateIssue.as_view()

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from webapp.views import IndexView, IssueView, CreateIssue, UpdateIssue, DeleteIssue
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('issue/', RedirectView.as_view(pattern_name='index')),
     path('issue/<int:pk>', IssueView.as_view(), name='IssueView'),
     path('issue/create/', CreateIssue.as_view(), name='CreateIssue'),
     path('issue/update/<int:pk>/', UpdateIssue.as_view(), name='UpdateIssue'),
