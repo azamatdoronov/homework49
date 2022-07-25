230420bc86144a5ea99183100685d17a3db09526
azamatdoronov <aza3el@inbox.ru>
2022-07-25 23:51:00 +0600

    Прописал поля формы для класса Project

--- webapp/forms.py
+++ webapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.forms import widgets
 
-from webapp.models import Issue
+from webapp.models import Issue, Project
 
 
 class IssueForm(forms.ModelForm):
@@ -21,3 +21,17 @@ class IssueForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=50, required=False, label='Поиск')
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['p_name', 'p_description', 'start_date', 'expiration_date']
+        widgets = {
+            "p_description": widgets.Textarea(attrs={"placeholder": "Введите описание"})
+        }
+
+    def clean(self):
+        if self.cleaned_data.get("p_name") == self.cleaned_data.get("p_description"):
+            raise ValidationError("Название и описание проекта не могут совпадать")
+        return super().clean()
